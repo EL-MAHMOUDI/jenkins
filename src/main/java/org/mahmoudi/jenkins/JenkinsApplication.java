eaa5504f8dc9ab8a0308fa449ff004683e762571
EL-MAHMOUDI <amine.el.mahmoudi@outlook.com>
2021-03-04 18:27:25 +0100

    Dummy commit

---
+++ src/main/java/org/mahmoudi/jenkins/JenkinsApplication.java
@@ -0,0 +1,19 @@
+package org.mahmoudi.jenkins;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner loader(){
+        return args -> System.out.println("Hello world!");
+    }
+}
